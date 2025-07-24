9ba5baca7406288e4a150b47ccf5b4b61dd198c9
Kanec Olivares <kanecolivares@gmail.com>
2025-07-24 15:01:11 -0700

    CMAKE finished. Compiled and ready to execute.

---
+++ build/CMakeFiles/main.dir/src/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/main.dir/src/main.cpp.o: \
+  /Users/kanecolivares/Personal/fun-scripts/Wieght-Loss-Python-Lib/src/main.cpp
---
+++ build/CMakeFiles/main.dir/src/person.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/main.dir/src/person.cpp.o: \
+  /Users/kanecolivares/Personal/fun-scripts/Wieght-Loss-Python-Lib/src/person.cpp
